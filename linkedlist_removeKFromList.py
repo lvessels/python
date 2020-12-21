9365926f988780cede3dbf5363657f35636ae258
lvessels <lre12a@acu.edu>
2020-12-21 10:38:25 -0500

    Create linkedlist_removeKFromList.py

---
+++ linkedlist_removeKFromList.py
@@ -0,0 +1,25 @@
+"""
+Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k.
+
+Example
+
+For l = [3, 1, 2, 3, 4, 5] and k = 3, the output should be
+removeKFromList(l, k) = [1, 2, 4, 5];
+For l = [1, 2, 3, 4, 5, 6, 7] and k = 10, the output should be
+removeKFromList(l, k) = [1, 2, 3, 4, 5, 6, 7].
+"""
+def removeKFromList(l, k):
+    if not l:
+        return l
+    
+    curr = l
+    while curr:
+        if curr.next and curr.next.value == k:
+            curr.next = curr.next.next
+        else:
+            curr = curr.next
+    
+    if l.value == k:
+        l = l.next
+    
+    return  l
