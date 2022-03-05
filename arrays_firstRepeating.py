a252f5984cc9cd9313584f2e1ec01fce8ac10165
lvessels <lre12a@acu.edu>
2022-03-05 00:53:31 -0500

    Fixing files where there are long lines

--- arrays_firstRepeating.py
+++ arrays_firstRepeating.py
@@ -1,5 +1,7 @@
 """
-Given a string s consisting of small English letters, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+Given a string s consisting of small English letters,
+find and return the first instance of a non-repeating character in it. 
+If there is no such character, return '_'.
 
 Example
 
--- linkedlist_reverseNodesInKGroups.py
+++ linkedlist_reverseNodesInKGroups.py
@@ -1,5 +1,8 @@
 """
-Given a linked list l, reverse its nodes k at a time and return the modified list. k is a positive integer that is less than or equal to the length of l. If the number of nodes in the linked list is not a multiple of k, then the nodes that are left out at the end should remain as-is.
+Given a linked list l, reverse its nodes k at a time and return the modified list.
+k is a positive integer that is less than or equal to the length of l.
+If the number of nodes in the linked list is not a multiple of k,
+then the nodes that are left out at the end should remain as-is.
 
 You may not alter the values in the nodes - only the nodes themselves can be changed.
 
